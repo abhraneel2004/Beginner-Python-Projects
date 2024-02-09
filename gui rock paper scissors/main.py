d888cb5605d3cb1eae36250246f40337074a6613
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-09 23:14:29 +0530

    Design modified

--- gui rock paper scissors/main.py
+++ gui rock paper scissors/main.py
@@ -8,21 +8,21 @@ def rock():
     global score
     k = randint(0, 2)
     com = 'Computer played:'+list1[k]+'  '+list2[k]
-    if score < 10 :
+    if score < 10:
         if k == 0:
             lbl.config(text=com)
             lbl1.config(text='Its a Draw...(✿◠‿◠)')
         elif k == 1:
-            score-=1
+            score -= 1
             lbl.config(text=com)
             sclbl.config(text=score)
             lbl1.config(text='You Loose....(´。＿。`)')
         elif k == 2:
-            score+=1
+            score += 1
             sclbl.config(text=score)
             lbl.config(text=com)
             lbl1.config(text='You Win !!!!....╰(*°▽°*)╯')
-    elif score==10:
+    elif score == 10:
         lbl.config(text='Game Over...You won')
 
 
@@ -32,7 +32,7 @@ def paper():
     com = 'Computer played:'+list1[k]+'  '+list2[k]
     if score < 10:
         if k == 0:
-            score+=1
+            score += 1
             sclbl.config(text=score)
             lbl.config(text=com)
             lbl1.config(text='You Win !!!!....╰(*°▽°*)╯')
@@ -40,11 +40,11 @@ def paper():
             lbl.config(text=com)
             lbl1.config(text='Its a Draw...(✿◠‿◠)')
         elif k == 2:
-            score-=1
+            score -= 1
             sclbl.config(text=score)
             lbl.config(text=com)
             lbl1.config(text='You Loose....(´。＿。`)')
-    elif score==10:
+    elif score == 10:
         lbl.config(text='Game Over...You won')
 
 
@@ -54,42 +54,48 @@ def scissor():
     com = list1[k]+'  '+list2[k]
     if score < 10:
         if k == 0:
-            score-=1
+            score -= 1
             sclbl.config(text=score)
             lbl.config(text=com)
             lbl1.config(text='You Loose....(´。＿。`)')
         elif k == 1:
-            score+=1
+            score += 1
             sclbl.config(text=score)
             lbl.config(text=com)
             lbl1.config(text='You Win !!!!....╰(*°▽°*)╯')
         elif k == 2:
             lbl.config(text=com)
             lbl1.config(text='Its a Draw...(✿◠‿◠)')
-    elif score==10:
+    elif score == 10:
         lbl.config(text='Game Over...You won')
+
 
 list1 = ['ROCK', 'PAPER', 'SCISSORS']
 list2 = ['✊', '🖐', '✌']
 
 window = Tk()
 window.title('Rock Paper Scissors')
-window.config(bg='yellow')
-sclbl = Label(window, text=score, font=('Arial', 30, 'bold'))
+window.config(bg='#FFB000')
+sclbl = Label(window, bg="#FFCF9D", text=score, font=('Arial', 30, 'bold'))
 sclbl.pack()
-lbl = Label(window, text='   ', font=('Arial', 30, 'bold'))
-lbl.pack(padx=10,pady=20)
-lbl1 = Label(window, text='   ', font=('Arial', 30, 'bold'))
-lbl1.pack(padx=10,pady=20)
-frame1 = Frame(window, bg='yellow', highlightbackground='red', highlightthickness='10')
-frame1.pack(padx=10,pady=10)
-lbl2 = Label(frame1, text='Click The Button To Play The Round', font=('Arial', 30, 'bold'))
+lbl = Label(window, bg="#FFCF9D", text='   ', font=('Arial', 30, 'bold'))
+lbl.pack(padx=10, pady=20)
+lbl1 = Label(window, bg="#FFCF9D", text='   ', font=('Arial', 30, 'bold'))
+lbl1.pack(padx=10, pady=20)
+frame1 = Frame(window, bg='#FFB000', highlightbackground='red',
+               highlightthickness='1')
+frame1.pack(padx=10, pady=10, ipadx=20, ipady=20)
+lbl2 = Label(frame1, bg="#FFB000",
+             text='Click The Button To Play The Round', font=('Arial', 30, 'bold'))
 lbl2.pack()
-btn = Button(frame1, text='Rock', bg='orange', fg='black', command=rock)
-btn.pack()
-btn = Button(frame1, text='Paper', bg='orange', fg='black', command=paper)
-btn.pack()
-btn = Button(frame1, text='Scissor', bg='orange', fg='black', command=scissor)
-btn.pack()
+btn = Button(frame1, text='Rock',font=('Arial', 21, 'bold'), bg='#004225', fg='#FFB000',
+             width=30, height=3,activeforeground='yellow', activebackground='#395144', command=rock)
+btn.pack(padx=10, pady=20)
+btn = Button(frame1, text='Paper',font=('Arial', 21, 'bold'), bg='#004225', fg='#FFB000',
+             width=30, height=3,activeforeground='yellow', activebackground='#395144', command=paper)
+btn.pack(padx=10, pady=20)
+btn = Button(frame1, text='Scissor',font=('Arial', 21, 'bold'), bg='#004225', fg='#FFB000',
+             width=30, height=3,activeforeground='yellow', activebackground='#395144', command=scissor)
+btn.pack(padx=10, pady=20)
 
 window.mainloop()
